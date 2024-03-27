331f7e6bc69339901b2c55702e565ef510deb931
amani-abushehade <abushehadaamani@gmail.com>
2024-03-27 15:22:53 +0300

    first commit

---
+++ src/compomets/Landing.js
@@ -0,0 +1,17 @@
+import { ArrowForward } from "@mui/icons-material";
+import { Button } from "@mui/material";
+
+function Landing() {
+  return (
+    <div className="landing">
+      <div className="content">
+        <div className="slogan">Professional. Fast. Reliable</div>
+        <div className="desc">
+          Laptopi is ready to help with all your appliance repair and service
+          needs.
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Landing;
